1d7cb6abf2e34fa0a289f35353d89539bd3d432e
Nikita <sirwior13@gmail.com>
2019-10-12 20:43:14 +0300

    DAl created

---
+++ Hospital.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hospital.WEB.MvcApplication" Language="C#" %>
---
+++ Hospital.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hospital.WEB.Startup))]
+namespace Hospital.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
